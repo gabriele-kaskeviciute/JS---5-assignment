701d072a406d3d0dc157bdc87280a4097b184289
gabriele-kaskeviciute <gabi.kaskeviciute@gmail.com>
2023-11-22 21:42:52 +0200

    DOM

---
+++ script.js
@@ -0,0 +1,17 @@
+
+function myFunction() {
+  var left = document.getElementById("right").value;
+  var right = document.getElementById("right").value;
+  var reset = document.getElementById("reset").value;
+
+
+
+
+  document.getElementById("left").innerHTML = left;
+  document.getElementById("right").innerHTML = right;
+  document.getElementById("reset").innerHTML = reset;
+
+
+
+
+}
